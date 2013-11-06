0a8718453679e6aeff0288f34e70440f1df518df
frankjwu <me@frankjwu.com>
2013-11-05 20:31:05 -0500

    skeleton

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	reset();
-	
+
 	window.onresize = function(event) {
     	reset();
 	}
@@ -9,4 +9,8 @@ $(document).ready(function() {
 function reset() {
 	$(".block").css("height", $(window).height());
 	$(".block").css("background-size", $(window).width() + "px " + $(window).height() + "px");
+	$("#b1").css("top", 0.9 * $(window).height() + "px");
+	$("#b2").css("top", 1.9 * $(window).height() + "px");
+	$(".content").css("padding", 0.05 * $(window).height() + "px");
+	$("#c1").css("padding-top", 0.20 * $(window).height() + "px");
 }
